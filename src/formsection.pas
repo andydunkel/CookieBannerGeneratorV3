a60e77d76f5e3513b47c6200fd81709a8c9735a1
Andy <andy.dunkel@ekiwi.de>
2025-10-28 10:29:22 +0100

    Dialog erstellt

--- src/formsection.pas
+++ src/formsection.pas
@@ -1,18 +1,24 @@
 unit formsection;
-
 {$mode ObjFPC}{$H+}
 
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
+  { TFormEditSection }
   TFormEditSection = class(TForm)
+    ButtonCancel: TButton;
+    ButtonOK: TButton;
+    ComboBoxLinkedCategory: TComboBox;
+    EditTitle: TEdit;
+    LabelDescription: TLabel;
+    LabelLinkedCategory: TLabel;
+    LabelTitle: TLabel;
+    MemoDescription: TMemo;
   private
-
   public
-
   end;
 
 var
@@ -23,4 +29,3 @@ implementation
 {$R *.lfm}
 
 end.
-

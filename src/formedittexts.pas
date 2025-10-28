e2d70eeb1755b999af5f69a44a0b5cd89d7c8820
Andy <andy.dunkel@ekiwi.de>
2025-10-28 10:27:20 +0100

    Dialog erstellt

--- src/formedittexts.pas
+++ src/formedittexts.pas
@@ -1,5 +1,4 @@
 unit formedittexts;
-
 {$mode ObjFPC}{$H+}
 
 interface
@@ -8,15 +7,43 @@ uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls;
 
 type
-
   { TFormEditLangTexts }
-
   TFormEditLangTexts = class(TForm)
     ButtonCatDelete: TToolButton;
     ButtonCatDown: TToolButton;
     ButtonCatEdit: TToolButton;
     ButtonCatNew: TToolButton;
     ButtonCatUp: TToolButton;
+    
+    // Consent Modal controls
+    EditConsentTitle: TEdit;
+    EditConsentAcceptAll: TEdit;
+    EditConsentRejectAll: TEdit;
+    EditConsentPrefs: TEdit;
+    LabelConsentTitle: TLabel;
+    LabelConsentDesc: TLabel;
+    LabelConsentAcceptAll: TLabel;
+    LabelConsentRejectAll: TLabel;
+    LabelConsentPrefs: TLabel;
+    LabelConsentFooter: TLabel;
+    ListViewBlocks: TListView;
+    MemoConsentDesc: TMemo;
+    MemoConsentFooter: TMemo;
+    
+    // Preferences Modal controls
+    EditPrefTitle: TEdit;
+    EditPrefAcceptAll: TEdit;
+    EditPrefRejectAll: TEdit;
+    EditPrefSave: TEdit;
+    EditPrefClose: TEdit;
+    EditPrefServiceCounter: TEdit;
+    LabelPrefTitle: TLabel;
+    LabelPrefAcceptAll: TLabel;
+    LabelPrefRejectAll: TLabel;
+    LabelPrefSave: TLabel;
+    LabelPrefClose: TLabel;
+    LabelPrefServiceCounter: TLabel;
+    
     GroupBoxConsentTexts: TGroupBox;
     GroupBoxPrefModal: TGroupBox;
     PageControl1: TPageControl;
@@ -25,9 +52,7 @@ type
     ToolBarCat: TToolBar;
     ToolButton6: TToolButton;
   private
-
   public
-
   end;
 
 var
@@ -38,4 +63,3 @@ implementation
 {$R *.lfm}
 
 end.
-
---
+++ src/formsection.pas
@@ -0,0 +1,26 @@
+unit formsection;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TFormEditSection = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  FormEditSection: TFormEditSection;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
